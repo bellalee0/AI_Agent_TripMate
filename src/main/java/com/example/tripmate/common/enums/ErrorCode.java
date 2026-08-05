3b1deb3eeadc5d6cf42029e64a618ff36e8a410f
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-05 23:00:58 +0900

    feat: 커스텀 예외 및 처리

---
+++ src/main/java/com/example/tripmate/common/enums/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.example.tripmate.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // 인증
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "유효 기간이 만료된 토큰입니다."),
+    INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    REFRESH_NOT_FOUND(HttpStatus.NOT_FOUND, "리프레시 토큰이 존재하지 않습니다."),
+    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 권한입니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
