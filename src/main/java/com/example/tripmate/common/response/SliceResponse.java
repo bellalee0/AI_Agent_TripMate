77995a68c3ce9bb346cb2ea36a34ed928716c640
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-05 23:01:29 +0900

    feat: Slice 커스텀 응답 포맷 생성

---
+++ src/main/java/com/example/tripmate/common/response/SliceResponse.java
@@ -0,0 +1,25 @@
+package com.example.tripmate.common.response;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+
+@Getter
+@RequiredArgsConstructor
+public class SliceResponse<T> {
+
+    private final List<T> content;
+    private final int size;
+    private final int number;
+    private final boolean hasNext;
+
+    public static <T> SliceResponse<T> from(Slice<T> slice) {
+        return new SliceResponse<>(
+                slice.getContent(),
+                slice.getSize(),
+                slice.getNumber(),
+                slice.hasNext()
+        );
+    }
+}
