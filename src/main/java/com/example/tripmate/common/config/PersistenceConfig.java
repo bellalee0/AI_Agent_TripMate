3894f252fa38aaea5d4b92ce9b52fd9593c3b4a9
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-06 15:49:01 +0900

    refactor: mongodb auditing 설정 추가

--- src/main/java/com/example/tripmate/common/config/PersistenceConfig.java
+++ src/main/java/com/example/tripmate/common/config/PersistenceConfig.java
@@ -2,9 +2,11 @@ package com.example.tripmate.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@EnableMongoAuditing
 public class PersistenceConfig {
 
 }
