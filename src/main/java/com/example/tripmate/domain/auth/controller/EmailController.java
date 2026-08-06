f2839dd73667b3fdd4c5a3ec7f4ea913cc957662
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-06 17:43:30 +0900

    feat: email 중복 검사 및 인증 코드 발송 API 생성

---
+++ src/main/java/com/example/tripmate/domain/auth/controller/EmailController.java
@@ -0,0 +1,43 @@
+package com.example.tripmate.domain.auth.controller;
+
+import com.example.tripmate.common.dto.CommonResponse;
+import com.example.tripmate.common.enums.SuccessMessage;
+import com.example.tripmate.domain.auth.dto.request.EmailVerificationRequest;
+import com.example.tripmate.domain.auth.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@Tag(name = "Auth")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    /**
+     * 이메일 중복 확인 및 인증번호 전송
+     */
+    @Operation(
+        summary = "이메일 중복 확인 및 인증번호 전송",
+        description = """
+                    이메일 중복을 확인하고, 사용자의 이메일에 인증번호를 전송합니다.
+                    """
+    )
+    @PostMapping("/email/verification-code")
+    public ResponseEntity<CommonResponse<Void>> sendVerificationEmail(@Valid @RequestBody EmailVerificationRequest request) {
+
+        emailService.sendVerificationEmail(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(CommonResponse.successNodata(SuccessMessage.AUTH_EMAIL_SEND_SUCCESS));
+    }
+}
---
+++ src/main/java/com/example/tripmate/domain/auth/dto/request/EmailVerificationRequest.java
@@ -0,0 +1,14 @@
+package com.example.tripmate.domain.auth.dto.request;
+
+import com.example.tripmate.common.constants.ValidationMessage;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class EmailVerificationRequest {
+
+    @NotBlank(message = ValidationMessage.EMAIL_NOT_BLANK)
+    @Email(message = ValidationMessage.EMAIL_FORMAT)
+    private String email;
+}
