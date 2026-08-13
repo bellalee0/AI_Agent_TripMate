c837619c824f75a2980703715ee866c5032fe6b0
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-13 23:50:06 +0900

    refactor: Request DTO 생성자 추가

--- src/main/java/com/example/tripmate/domain/auth/dto/request/AuthSignupRequest.java
+++ src/main/java/com/example/tripmate/domain/auth/dto/request/AuthSignupRequest.java
@@ -4,9 +4,13 @@ import com.example.tripmate.common.constants.ValidationMessage;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthSignupRequest {
 
     @NotBlank(message = ValidationMessage.USERNAME_NOT_BLANK)
