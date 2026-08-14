3bf204dec6c3d36482c0a4258cafa5c7e02f50c3
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-14 19:16:39 +0900

    feat: Swagger Group 태그 추가

--- src/main/java/com/example/tripmate/common/config/SwaggerConfig.java
+++ src/main/java/com/example/tripmate/common/config/SwaggerConfig.java
@@ -25,7 +25,8 @@ public class SwaggerConfig {
         return new OpenAPI()
             .tags(List.of(
                 new Tag().name("Auth").description("인증/인가를 다루는 API입니다."),
-                new Tag().name("User").description("유저 정보 관리를 다루는 API입니다.")
+                new Tag().name("User").description("유저 정보 관리를 다루는 API입니다."),
+                new Tag().name("Group").description("그룹 관리를 다루는 API입니다.")
             ))
             .addSecurityItem(new SecurityRequirement().addList("JWT"))
             .components(new Components()
