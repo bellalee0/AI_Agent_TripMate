e9eb5c5dc0daae60a7fa78a671fa1dfef19fcb3c
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-08-14 16:44:46 +0900

    test: UserService 메서드 단위 테스트

--- src/test/java/com/example/tripmate/fixture/UserFixture.java
+++ src/test/java/com/example/tripmate/fixture/UserFixture.java
@@ -1,5 +1,6 @@
 package com.example.tripmate.fixture;
 
+import com.example.tripmate.common.dto.AuthUser;
 import com.example.tripmate.common.entity.User;
 import com.example.tripmate.common.enums.UserRole;
 
@@ -7,5 +8,9 @@ public class UserFixture {
 
     public static User testUser() {
         return new User("test@test.com", "test", "testUser", "password1234", null, UserRole.USER);
+    }
+
+    public static AuthUser testAuthUser() {
+        return new AuthUser(1L, "test@test.com", UserRole.USER);
     }
 }
